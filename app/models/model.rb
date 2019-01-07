fe0c668348c2d20337cf041f7a51cf1ae66ff5c1
Tara Sabella <tarasabella@gmail.com>
2019-01-07 21:56:49 +0000

    Automatically backed up by Learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,13 @@
+<<<<<<< HEAD
 class Model
   
 end 
+=======
+class Dog < ApplicationController
+
+  @dog = Dog.create(:name: => params[:name], :age: => params[:age], :breed: => params[:breed])
+  #dog.save
+
+  end
+end  
+>>>>>>> 7e4d7b7561f706f2fa82c5be4cd0df998da05a4d
